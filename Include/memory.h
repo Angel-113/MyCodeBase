f5ca8f743f1fe7b81f4e80dc503689efc72f9531
Angel-B113 <angel.ricahalo3@gmail.com>
2024-08-19 23:51:55 -0600

    Arena implementation is almost finished - Gotta see how to securely extend an arena

--- Include/memory.h
+++ Include/memory.h
@@ -5,4 +5,19 @@
 #ifndef MYCODEBASE_MEMORY_H
 #define MYCODEBASE_MEMORY_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct Arena {
+    char* arena;
+    size_t size;
+    size_t offset;
+} Arena;
+
+extern Arena* CreateArena ( size_t size );
+extern void ExtendArena ( Arena* arena );
+extern void* ArenaAlloc ( Arena* A, size_t size );
+extern void DestroyArena ( Arena* A );
+
 #endif //MYCODEBASE_MEMORY_H
--- Src/memory.c
+++ Src/memory.c
@@ -1,3 +1,43 @@
 //
 // Created by angel on 19/08/24.
 //
+
+#include "memory.h"
+
+Arena* CreateArena ( size_t size ) {
+    Arena* arena = (Arena*) malloc(sizeof(Arena));
+    if ( arena != NULL ) {
+        arena->arena = (char *) malloc(size);
+        if ( arena->arena == NULL ) {
+            perror("Error when creating Arena\n");
+            return NULL;
+        }
+        arena->size = size;
+        arena->offset = 0;
+    }
+    return arena;
+}
+
+void* ArenaAlloc ( Arena* A, size_t size ) {
+    if ( A->offset + size > A->size ) {
+        perror("This arena is not big enough");
+        return NULL;
+    }
+    void* ptr = A->arena + A->offset;
+    A->offset += size;
+    return ptr;
+}
+
+void ExtendArena ( Arena* arena ) { /* This function supposes that arena has reached its maximum capacity */
+
+    /* Still thinking in how to extend an arena */
+
+}
+
+void DestroyArena ( Arena* A ) {
+    free ( A->arena );
+    A->offset = 0;
+    A->size = 0;
+    free ( A );
+    A = NULL;
+}
