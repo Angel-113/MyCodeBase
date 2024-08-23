f62f81ba20efd62178a49a62a13638206cc071f5
Angel-113 <angel.ricahalo3@gmail.com>
2024-08-23 12:00:03 -0600

    Linked List - I'm starting to dev all Linked List implementations such as DLL, CLL, SLL (Stack), SLL (Queue)

--- Include/dsa.h
+++ Include/dsa.h
@@ -5,4 +5,73 @@
 #ifndef MYCODEBASE_DSA_H
 #define MYCODEBASE_DSA_H
 
+#include "memory.h"
+
+typedef enum Type {
+    INT = 0,
+    FLOAT = 1,
+    DOUBLE = 2,
+    STRING = 3,
+    VAR = 4 /* a void* not recommendable if you're not allocating structures in the node */
+} Type;
+
+/*
+ * It's better to create from scratch an LLStruct with a struct data you want to use
+ * instead of using the LLVar. That's often better for memory and execution performance.
+ */
+
+typedef struct NodeInt {
+    struct NodeInt* prev;
+    int data;
+    struct NodeInt* next;
+} NodeInt;
+
+typedef struct NodeFloat {
+    struct NodeFloat* prev;
+    float data;
+    struct NodeFloat* next;
+} NodeFloat;
+
+typedef struct NodeDouble {
+    struct NodeDouble* prev;
+    double data;
+    struct NodeDouble* next;
+} NodeDouble;
+
+typedef struct NodeString {
+    struct NodeString* prev;
+    char* data;
+    struct NodeString* next;
+} NodeString;
+
+typedef struct NodeVar {
+    struct NodeVar* prev;
+    void* data;
+    struct NodeVar* next;
+} NodeVar;
+
+typedef struct LL {
+    Arena* memory_pool;
+    void* head;
+    Type type;
+    size_t size;
+    void* tail;
+} LL;
+
+typedef struct CLL {
+    Arena* memory_pool;
+    void* head;
+    Type type;
+    size_t size;
+} CLL;
+
+/* Linked List Functions */
+extern LL* InitList ( void* data, Type t ); /* Init Linked List */
+extern void AddNode ( LL* list, void* data ); /* Add node to head */
+extern void RemoveNode ( void* Tail, void* data, Type t ); /* Remove node to tail */
+extern void* GetLastNode (void* Tail); /* Return ptr to last node */
+
+/* Circular Linked List Functions */
+
+
 #endif //MYCODEBASE_DSA_H
--- Src/dsa.c
+++ Src/dsa.c
@@ -1,3 +1,85 @@
 //
 // Created by angel on 19/08/24.
 //
+
+#include "dsa.h"
+/* Node Implementations */
+static void* InitNode ( void* node, void* data, Type t );
+static void InitNodeInt ( NodeInt* node, int data );
+static void InitNodeFloat ( NodeFloat* node, float data );
+static void InitNodeDouble ( NodeDouble* node, double data );
+static void InitNodeString ( NodeString* node, char* data );
+static void InitNodeVar ( NodeVar* node, void* data );
+static void PrintNode ( void* node, Type t );
+
+static void* InitNode ( void* node, void* data, const Type t ) {
+    switch (t) {
+        default:break;
+        case INT:
+            InitNodeInt(node, *((int*)data));
+        break;
+        case FLOAT:
+            InitNodeFloat(node, *((float*)data));
+        break;
+        case DOUBLE:
+            InitNodeDouble(node, *((double*)data));
+        break;
+        case STRING:
+            InitNodeString(node, (char*)data);
+        break;
+        case VAR:
+            InitNodeVar(node, data);
+        break;
+    }
+    return node;
+}
+
+static void InitNodeInt ( NodeInt* node, const int data ) {
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+}
+
+static void InitNodeFloat ( NodeFloat* node, const float data ) {
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+}
+
+static void InitNodeDouble ( NodeDouble* node, const double data ) {
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+}
+
+static void InitNodeString ( NodeString* node, char* data ) {
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+}
+
+static void InitNodeVar ( NodeVar* node, void* data ) {
+    node->data = data;
+    node->next = NULL;
+    node->prev = NULL;
+}
+
+static void PrintNode ( void* node, Type t ) {
+
+}
+
+/* Linked List Implementations */
+LL* InitList ( void* data, const Type t ) {
+    LL* list = malloc(sizeof(LL));
+
+    list->memory_pool = CreateArena(10 * sizeof(NodeVar));
+    list->head = ArenaAlloc(list->memory_pool, sizeof(NodeVar), DYNAMIC);
+    list->head = InitNode(list->head, data, t);
+    list->size += 1;
+    list->type = t;
+
+    const NodeVar* tmp = list->head;
+    list->tail = tmp->next;
+
+    return list;
+}
