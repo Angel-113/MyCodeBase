556cc9f88c1dcf8be5c39d2f7d048294db29bdb1
Angel-B113 <angel.ricahalo3@gmail.com>
2024-10-08 01:50:48 -0600

    Nothing to say just some general changes

--- Include/base.h
+++ Include/base.h
@@ -18,15 +18,13 @@
     if ( (s1) != NULL && (s2) != NULL ) \
         (s1) = (s2);
 
-typedef unsigned char byte; /* 2 ^ 8 */
-typedef unsigned long long int uint64; /* 2 ^ 64 */
-typedef unsigned int uint32;
+typedef unsigned char byte;
+typedef unsigned int uint64;
 typedef unsigned short uint16;
 typedef unsigned char uint8;
 typedef char int8;
 typedef short int int16;
-typedef long int int32;
-typedef long long int int64; /* 2 ^ 64 - 1 */
+typedef long int int64;
 
 typedef char* string;
 typedef void* object; /* ----> When using it as a pointer to any type or any struct */
